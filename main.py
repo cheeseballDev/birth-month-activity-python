851e283ec5ecea1c4c68a48be612571a5e4ac25b
Matthew <cheeseballbusinessemail@gmail.com>
2024-11-15 18:07:30 +0800

    Fixed the for loop statement to not count if month is invalid

--- main.py
+++ main.py
@@ -2,18 +2,46 @@ from months import Months
 
 group1 = set()
 group2 = set()
+self = set()
 union = set()
 
 def main() -> None:
-    for i in range(3):
-        group1.add(input(f"Enter birth month {i + 1}: ".format(i)))
-    for i in range(3):
-        group2.add(input(f"Enter birth month {i + 1}: ".format(i)))
+    i = 0
+    l = 0
+    while i < 3:
+        month = input(f"Enter birth month {i+1}: ".format(i))
+        if not (is_birth_month(month)):
+            print("Invalid birth month. Please try again!")
+            continue
+        group1.add(month)
+        i += 1
+    while l < 3:
+        month = input(f"Enter birth month {l+1}: ".format(l))
+        if not (is_birth_month(month)):
+            print("Invalid birth month. Please try again!")
+            continue
+        group2.add(month)
+        l += 1
     print(group1)
     print(group2)
+    self.add(input(f"Enter your birth month: "))
+    calculate_set()
+    if self.issubset(union):
+        print("You have the same birth month with one of your classmates.")
+    else:
+        print("You don't have the same birth month with any of your classmates.")
 
-def isBirthMonth() -> bool:
-    pass
+def calculate_set() -> None:
+    union = str(group1 | group2)
+    print("Union: " + union)
+    print("Intersection: " + str(group1 & group2))
+    print("Difference: " + str(group1 - group2))
+
+def is_birth_month(userMonth) -> bool:
+    for month in Months.months:
+        if (userMonth == month):
+            return True
+    return False
 
 if __name__ == "__main__":
     main()
